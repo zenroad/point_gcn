3f93929812cd07558e485b9c27ca1bbd1c062528
zhongchen <1518595517@qq.com>
2019-03-04 17:48:12 +0800

    11

--- train.py
+++ train.py
@@ -11,12 +11,39 @@ from torch_geometric.datasets import ModelNet
 from torch_geometric.transforms import SamplePoints
 from torch_geometric.transforms import FaceToEdge
 import torch_geometric
+from torch_geometric.utils import to_undirected
 
 
 
 class MyTransform(object):
     def __call__(self, data):
-        data.face, data.x = None, torch.ones(data.num_nodes, 1)
+        #data.face, data.x = None, torch.ones(data.num_nodes, 1)
+        num = 1024
+        pos,face = data.pos,data.face
+        assert not pos.is_cuda and not face.is_cuda
+        assert pos.size(1) == 3 and face.size(0) == 3
+
+        area = (pos[face[1]] - pos[face[0]]).cross(pos[face[2]] - pos[face[0]])
+        area = torch.sqrt((area**2).sum(dim=-1)) / 2
+        
+        prob = area / area.sum()
+        sample = torch.multinomial(prob, num, replacement=True)
+        face = face[:, sample]
+
+        frac = torch.rand(num, 2)
+        mask = frac.sum(dim=-1) > 1
+        frac[mask] = 1 - frac[mask]
+
+        pos_sampled = pos[face[0]]
+        pos_sampled += frac[:, :1] * (pos[face[1]] - pos[face[0]])
+        pos_sampled += frac[:, 1:] * (pos[face[2]] - pos[face[0]])
+
+        data.pos = pos_sampled
+        
+        edge_index = torch.cat([face[:2], face[1:], face[::2]], dim=1)
+        edge_index = to_undirected(edge_index, num_nodes=data.num_nodes)
+        data.edge_index = edge_index
+        data.face = None
         return data
 
 #path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'MUTAG')
