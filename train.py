d5be7cae43147cdf6230ac5cb5edf5c4988d5eb2
zhongchen <1518595517@qq.com>
2019-03-04 15:05:22 +0800

    init

---
+++ train.py
@@ -0,0 +1,86 @@
+import os.path as osp
+
+import torch
+import torch.nn.functional as F
+from torch.nn import Sequential, Linear, ReLU
+#from torch_geometric.datasets import TUDataset
+from torch_geometric.data import DataLoader
+from torch_geometric.nn import GCNConv, global_add_pool
+import torch_geometric.transforms as T
+from torch_geometric.datasets import ModelNet
+from torch_geometric.transforms import SamplePoints
+from torch_geometric.transforms import FaceToEdge
+
+#path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'MUTAG')
+#dataset = TUDataset(path, name='MUTAG').shuffle()
+train_dataset = ModelNet(root='/home/code/geo/point_gcn/ModelNet',name='10',train=True, 
+                    pre_transform=FaceToEdge)
+test_dataset = ModelNet(root='/home/code/geo/point_gcn/ModelNet',name='10',train=False, 
+                    pre_transform=FaceToEdge)
+#test_dataset = dataset[:len(dataset) // 10]
+#train_dataset = dataset[len(dataset) // 10:]
+test_loader = DataLoader(test_dataset, batch_size=32)
+train_loader = DataLoader(train_dataset, batch_size=32)
+
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(train_dataset.num_features, 16)
+        self.conv2 = GCNConv(16, train_dataset.num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+
+        return F.log_softmax(x, dim=1)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+
+def train(epoch):
+    model.train()
+
+    if epoch == 51:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.5 * param_group['lr']
+
+    loss_all = 0
+    for data in train_loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        output = model(data.x, data.edge_index, data.batch)
+        loss = F.nll_loss(output, data.y)
+        loss.backward()
+        loss_all += loss.item() * data.num_graphs
+        optimizer.step()
+    return loss_all / len(train_dataset)
+
+
+def test(loader):
+    model.eval()
+
+    correct = 0
+    for data in loader:
+        data = data.to(device)
+        output = model(data.x, data.edge_index, data.batch)
+        pred = output.max(dim=1)[1]
+        correct += pred.eq(data.y).sum().item()
+    return correct / len(loader.dataset)
+
+
+for epoch in range(1, 101):
+    train_loss = train(epoch)
+    train_acc = test(train_loader)
+    test_acc = test(test_loader)
+    print('Epoch: {:03d}, Train Loss: {:.7f}, '
+          'Train Acc: {:.7f}, Test Acc: {:.7f}'.format(epoch, train_loss,
+                                                       train_acc, test_acc))
+
